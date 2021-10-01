ba2836406f4de824ddbb05354970e7d1ec8d078e
Evgeniy-Om <evgenicuss@gmail.com>
2021-10-01 16:44:03 +0300

    рефакторинг

--- src/core/hooks/index.ts
+++ src/core/hooks/index.ts
@@ -1,5 +1,5 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
-import type { RootState, AppDispatch } from '../types'
+import {TypedUseSelectorHook, useDispatch, useSelector} from 'react-redux'
+import type {AppDispatch, RootState} from '../types'
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = () => useDispatch<AppDispatch>()
--- src/core/store/birthdaySlice.ts
+++ src/core/store/birthdaySlice.ts
@@ -1,15 +1,8 @@
 import {createSlice, current, PayloadAction} from '@reduxjs/toolkit'
-import { SubmitPropsType } from '../../pages/New'
+import {SubmitPropsType} from '../../pages/New'
+import {BirthdayStateType} from '../types'
 
-export interface BirthdayState {
-    list: {
-        _id: number,
-        name: string
-        date: number | null
-    }[]
-}
-
-const initialState: BirthdayState = {
+const initialState: BirthdayStateType = {
     list: [
         {_id: 1, name: 'Таня Грин (Олайнфарм)', date: 216080395000},
         {_id: 2, name: 'Антон Автовинил', date: 416080000000},
@@ -30,19 +23,13 @@ export const birthdaySlice = createSlice({
             console.log(current(state))
             console.log(action)
         },
-        deleteBirthdayItem: (state, action: PayloadAction<number> ) => {
+        deleteBirthdayItem: (state, action: PayloadAction<number>) => {
             state.list.splice(action.payload, 1)
-        },
-        decrement: (state) => {
-            // state.value -= 1
-        },
-        incrementByAmount: (state, action: PayloadAction<number>) => {
-            // state.value += action.payload
         },
     },
 })
 
 // Action creators are generated for each case reducer function
-export const {changeBirthdayItem, deleteBirthdayItem, addBirthdayItem, decrement, incrementByAmount} = birthdaySlice.actions
+export const {changeBirthdayItem, deleteBirthdayItem, addBirthdayItem} = birthdaySlice.actions
 
 export default birthdaySlice.reducer
