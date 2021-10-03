f396129982dba4aace309e1db81066883fae2428
Evgeniy-Om <evgenicuss@gmail.com>
2021-10-03 16:55:16 +0300

    вынесение стилей в styled components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,67 +1,49 @@
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
-import AddIcon from '@mui/icons-material/Add'
-import DeleteIcon from '@mui/icons-material/Delete'
-import EditIcon from '@mui/icons-material/Edit'
+import MUIArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
+import MUIAddIcon from '@mui/icons-material/Add'
+import MUIDeleteIcon from '@mui/icons-material/Delete'
+import MUIEditIcon from '@mui/icons-material/Edit'
 import {useAppDispatch, useAppSelector} from "../core/hooks"
 import format from 'date-fns/format'
 import {ru} from 'date-fns/locale'
-import {Box, Button, IconButton} from "@mui/material"
-import {Link} from "react-router-dom"
-import {css} from '@emotion/css'
+import {Button as MUIButton, IconButton as MUIIconButton, styled} from "@mui/material"
+import {Link as ReactRouterDomLink} from "react-router-dom"
 import {deleteBirthdayItem} from '../core/store/birthdaySlice'
 
 
-function Home() {
+export default function Home() {
     const {list} = useAppSelector((state) => state.birthdays)
     const dispatch = useAppDispatch()
 
     return (
         <>
-            <Box sx={{
-                display: 'flex',
-                justifyContent: 'space-between',
-            }}>
-                <Button variant="outlined" startIcon={<ArrowBackIosIcon/>}>
-                    Выйти из приложения
-                </Button>
+            <Styled.Header>
+                <Styled.Link to="/login">
+                    <MUIButton variant="outlined" startIcon={<MUIArrowBackIosIcon/>}>
+                        Выйти из приложения
+                    </MUIButton>
+                </Styled.Link>
 
-                <Link to="/new" className={css({textDecoration: "none"})}>
-                    <Button variant="outlined" component="span" endIcon={<AddIcon/>}>
+                <Styled.Link to="/new">
+                    <MUIButton variant="outlined" component="span" endIcon={<MUIAddIcon/>}>
                         Новая запись
-                    </Button>
-                </Link>
-            </Box>
+                    </MUIButton>
+                </Styled.Link>
+            </Styled.Header>
             <h2>Birthday book</h2>
             <hr/>
             {list.map((item, index) =>
-                <Box key={index} sx={{
-                    display: "flex",
-                    alignItems: "center",
-                    marginBottom: "10px",
-
-                }}>
-                    <Box sx={{
-                        display: "flex",
-                        justifyContent: "space-between",
-                        alignItems: "center",
-                        height: "40px",
-                        width: "100%",
-                        marginRight: '5px',
-                        padding: "0px 10px",
-                        border: "1px solid",
-                        borderColor: theme => theme.palette.primary.light,
-                        borderRadius: theme => theme.shape.borderRadius,
-                    }}>
+                <Styled.ItemContainer key={index + Number(new Date())}>
+                    <Styled.NameAndDateContainer>
                         <span>{item.name}</span>
                         <span>{item.date && format(item.date, "d MMMM yyyy", {locale: ru})}</span>
-                    </Box>
-                    <IconButton aria-label="edit">
-                        <EditIcon sx={{color: theme => theme.palette.primary.light}}/>
-                    </IconButton>
-                    <IconButton aria-label="delete" onClick={() => dispatch(deleteBirthdayItem(index))}>
-                        <DeleteIcon sx={{color: theme => theme.palette.primary.light}}/>
-                    </IconButton>
-                </Box>,
+                    </Styled.NameAndDateContainer>
+                    <MUIIconButton aria-label="edit">
+                        <Styled.EditIcon/>
+                    </MUIIconButton>
+                    <MUIIconButton aria-label="delete" onClick={() => dispatch(deleteBirthdayItem(index))}>
+                        <Styled.DeleteIcon/>
+                    </MUIIconButton>
+                </Styled.ItemContainer>,
             )}
             <br/>
             <br/>
@@ -69,4 +51,36 @@ function Home() {
     )
 }
 
-export default Home
+const Styled = {
+    Header: styled("div")({
+        display: 'flex',
+        justifyContent: 'space-between',
+    }),
+    Link: styled(ReactRouterDomLink)({
+        textDecoration: "none"
+    }),
+    ItemContainer: styled("div")({
+        display: "flex",
+        alignItems: "center",
+        marginBottom: "10px",
+    }),
+    NameAndDateContainer: styled("div")(({theme}) => ({
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        height: "40px",
+        width: "100%",
+        marginRight: '5px',
+        padding: "0px 10px",
+        border: "1px solid",
+        borderColor: theme.palette.primary.light,
+        borderRadius: theme.shape.borderRadius,
+    })),
+    EditIcon: styled(MUIEditIcon)(({theme}) => ({
+        color: theme.palette.primary.light,
+    })),
+    DeleteIcon: styled(MUIDeleteIcon)(({theme}) => ({
+        color: theme.palette.primary.light,
+    })),
+
+}
