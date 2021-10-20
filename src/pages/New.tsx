1ec5fca6302c0d312b0dbd61eeaa8e3a506dba47
Evgeniy-Om <evgenicuss@gmail.com>
2021-10-20 20:07:56 +0300

    настройка страницы New (в процессе)

--- src/pages/New.tsx
+++ src/pages/New.tsx
@@ -4,30 +4,35 @@ import {DatePicker, LocalizationProvider} from "@mui/lab"
 import AdapterDateFns from "@mui/lab/AdapterDateFns"
 import ruLocale from "date-fns/locale/ru"
 import {addBirthdayItem, changeBirthdayItem} from "../core/store/birthdaySlice"
-import {Button, TextField} from "@mui/material"
+import {Button, Checkbox as MUICheckbox, FormControlLabel, styled, TextField} from "@mui/material"
 import {css} from "@emotion/css"
 import {Controller, useForm} from "react-hook-form"
 import ReactRouterDomLink from "../components/ReactRouterDomLink"
+import {Link} from "react-router-dom"
+import {NewCongratulationInputs} from "../core/global-types"
 
 export type SubmitPropsType = {
-    name: string,
+    name: string
     date: number
+    isCheckedEmail: boolean
 }
 
 export default function New() {
-    const {list, editId} = useAppSelector((state) => state.birthdays)
+    // const {list, editId} = useAppSelector((state) => state.birthdays)
 
-    const {register, handleSubmit, control} = useForm<Props>({
-        defaultValues: editId ? list.find(item => item._id === editId) : {date: null}
+    const {register, handleSubmit, control} = useForm<NewCongratulationInputs>({
+        // defaultValues: editId ? list.find(item => item._id === editId) : {date: null}
+        defaultValues: {alert_datetime: null}
     })
-    const dispatch = useAppDispatch()
+    // const dispatch = useAppDispatch()
 
-    const onSubmit = (data: SubmitPropsType) => {
-        dispatch(addBirthdayItem(data))
+    const onSubmit = (data: NewCongratulationInputs) => {
+        // dispatch(addBirthdayItem(data))
+        console.log(data)
     }
     return (
         <div>
-            <ReactRouterDomLink to="/" className={css({textDecoration: "none"})}>
+            <ReactRouterDomLink to="/">
                 <Button variant="outlined" component="span" startIcon={<ArrowBackIosIcon/>}>
                     Назад
                 </Button>
@@ -41,14 +46,14 @@ export default function New() {
             <form onSubmit={handleSubmit(onSubmit)}>
                 {/*React Hook Form контролирует DatePicker из Material UI, который в свою очередь рендерит TextField*/}
                 <Controller
-                    name="date"
+                    name="alert_datetime"
                     rules={{required: true}}
                     control={control}
                     render={({field: {value = "", onChange}}) =>
                         <LocalizationProvider dateAdapter={AdapterDateFns} locale={ruLocale}>
                             <DatePicker
                                 disableFuture
-                                label="Дата рождения *"
+                                label="Когда поздравить?"
                                 openTo="year"
                                 mask="__.__.____"
                                 views={['year', 'month', 'day']}
@@ -65,17 +70,22 @@ export default function New() {
                         </LocalizationProvider>
                     }
                 />
-
-
-                <TextField {...register("name")} variant="outlined" placeholder="Имя именинника"/>
+                <TextField {...register("bday_name")} variant="outlined" placeholder="Кого поздравить?"/>
+                <Styled.Label
+                    control={<MUICheckbox {...register("notify_by_email")} defaultChecked/>}
+                    label="Уведомить по e-mail"/>
+                <Styled.Label
+                    control={<MUICheckbox {...register("notify_by_push")} disabled/>}
+                    label="Отправить push-уведомление"/>
                 <Button type="submit">Отправить</Button>
             </form>
         </div>
     )
 }
 
-type Props = {
-    _id?: number,
-    name?: string,
-    date: number | null
+// Styled Components
+const Styled = {
+    Label: styled(FormControlLabel)({
+        margin: "0 auto 10px",
+    }),
 }
