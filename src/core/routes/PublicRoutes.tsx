9180accdfb2e1e7939129226f4b131e11f46bbeb
Evgeniy-Om <evgenicuss@gmail.com>
2021-10-26 19:47:05 +0300

    добавление роута ConfirmEmail

--- src/core/routes/PublicRoutes.tsx
+++ src/core/routes/PublicRoutes.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {BrowserRouter as Router, Redirect, Route, Switch} from "react-router-dom"
 import Login from "../../pages/Login"
 import Registration from "../../pages/Registration"
+import ConfirmEmail from "../../pages/ConfirmEmail"
 
 export default function PublicRoutes() {
     return (
@@ -12,6 +13,9 @@ export default function PublicRoutes() {
                 </Route>
                 <Route path="/registration">
                     <Registration/>
+                </Route>
+                <Route path="/confirm">
+                    <ConfirmEmail/>
                 </Route>
                 <Redirect to="/login"/>
             </Switch>
