cb238846f0ac7b0a8044ddf8e492d5a8f9549f59
Evgeniy-Om <evgenicuss@gmail.com>
2021-10-27 10:28:52 +0300

    удаление роутов Login и Registration из приватных

--- src/core/routes/PrivateRoutes.tsx
+++ src/core/routes/PrivateRoutes.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 import {BrowserRouter as Router, Redirect, Route, Switch} from "react-router-dom"
 // import Edit from "../../pages/Edit"
 import New from "../../pages/New"
-import Login from "../../pages/Login"
-import Registration from "../../pages/Registration"
 import Agreement from "../../pages/Agreement"
 import Home from "../../pages/Home"
 
@@ -11,18 +9,18 @@ export default function PrivateRoutes() {
     return (
         <Router>
             <Switch>
-                <Route path="/login">
-                    <Login/>
-                </Route>
+                {/*<Route path="/login">*/}
+                {/*    <Login/>*/}
+                {/*</Route>*/}
                 {/*<Route path="/edit">*/}
                 {/*    <Edit/>*/}
                 {/*</Route>*/}
                 <Route path="/new">
                     <New/>
                 </Route>
-                <Route path="/registration">
-                    <Registration/>
-                </Route>
+                {/*<Route path="/registration">*/}
+                {/*    <Registration/>*/}
+                {/*</Route>*/}
                 <Route path="/agreement">
                     <Agreement/>
                 </Route>
