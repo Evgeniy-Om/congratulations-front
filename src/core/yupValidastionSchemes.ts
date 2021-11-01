9fcba99d94c1d988729e29fb2a3394750bf87ec8
Evgeniy-Om <evgenicuss@gmail.com>
2021-11-01 10:17:19 +0300

    fix bags (STW-42, STW-43)

--- src/core/yupValidastionSchemes.ts
+++ src/core/yupValidastionSchemes.ts
@@ -14,12 +14,12 @@ export const loginValidationSchema: SchemaOf<LoginFormInputs> = object().shape({
 export const registrationValidationSchema: SchemaOf<RegistrationFormInputs> = object().shape({
     email: string()
         .required("Обязательное поле")
-        .max(100, 'Email должен быть меньше 100 символов')
+        .max(100, 'Email должен быть не более 100 символов')
         .matches(/^(?<user>([a-z0-9]([a-z0-9-]{0,61}[a-z0-9])?\.?)+[a-z0-9]+)@(?<nameSite>([A-Z0-9]([A-Z0-9-]{0,61}[A-Z0-9])?\.)+)(?<topLevelDomain>[A-Z]{2,63})$/, "Неверный формат эл. почты"),
     password: string()
         .required("Обязательное поле")
         .min(6, 'Пароль должен быть не менее 6 символов')
-        .max(50, 'Пароль должен быть меньше 50 символов'),
+        .max(50, 'Пароль должен быть не более 50 символов'),
     repeat: string()
         .required("Обязательное поле")
         .oneOf([ref('password'), null], 'Пароль не совпадает'),
