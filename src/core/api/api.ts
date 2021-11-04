dfdd524d8df78d029749120f97e2aea1479d9861
Evgeniy-Om <evgenicuss@gmail.com>
2021-11-04 09:05:01 +0300

    смена IP сервера

--- src/core/api/api.ts
+++ src/core/api/api.ts
@@ -2,8 +2,8 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/dist/query/react'
 
 export const emptySplitApi = createApi({
     baseQuery: fetchBaseQuery({
-        // baseUrl: 'https://birthdayappremainder.herokuapp.com/',
-        baseUrl: 'http://23.88.98.9:8010/'
+        baseUrl: 'https://birthdayappremainder.herokuapp.com/',
+        // baseUrl: 'http://23.88.98.9:8010/'
     }),
     tagTypes: ["Access", "Congratulations", 'Refresh'],
     endpoints: () => ({}),
