c74b91b7252f28703a3f7adac2e2a13dc74d5f77
Evgeniy-Om <evgenicuss@gmail.com>
2021-11-05 14:14:00 +0300

    вынос констант в один файл

---
+++ src/core/constants.tsx
@@ -0,0 +1,3 @@
+export const BASE_URL = 'https://birthdayappremainder.herokuapp.com/' // 'http://23.88.98.9:8010/'
+export const EMAIL_DEFAULT = "evgenicuss@gmail.com"
+export const PASSWORD_DEFAULT = "123212d"
