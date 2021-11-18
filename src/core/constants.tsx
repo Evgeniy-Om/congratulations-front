050b31e3faadd772e1467e80273ee29a31317c71
Evgeniy-Om <evgenicuss@gmail.com>
2021-11-18 16:37:20 +0300

    изменение URL сервера

--- src/core/constants.tsx
+++ src/core/constants.tsx
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://birthdayappremainder.herokuapp.com/' // 'http://23.88.98.9:8010/'
+export const BASE_URL = 'https://bdaybook.ru/'  // 'https://birthdayappremainder.herokuapp.com/'
 export const EMAIL_DEFAULT = "evgenicuss@gmail.com"
 export const PASSWORD_DEFAULT = "123212d"
 export const MIN_TIME_OF_LIVE_ACCESS_TOKEN = 65000
