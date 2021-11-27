b35f7bef61b33a9c4be20daf2ca104a350282739
Evgeniy-Om <evgenicuss@gmail.com>
2021-11-27 12:26:34 +0300

    удаление дефолтных значение логина и пароля

--- src/core/constants.tsx
+++ src/core/constants.tsx
@@ -1,5 +1,5 @@
 export const BASE_URL = 'http://localhost:8000/'  // 'https://birthdayappremainder.herokuapp.com/'
-export const EMAIL_DEFAULT = "evgenicuss@gmail.com"
-export const PASSWORD_DEFAULT = "123212d"
+export const EMAIL_DEFAULT = ""//"evgenicuss@gmail.com"
+export const PASSWORD_DEFAULT = ""//"123212d"
 export const MIN_TIME_OF_LIVE_ACCESS_TOKEN = 65000
 export const CHECK_VALID_ACCESS_TOKEN_INTERVAL = 60000
